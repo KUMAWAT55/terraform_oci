685368c7cf6949b9ed2023b1927308cf2acb1b5a
lastray_rk <RHTKUMAWAT55@GMAIL.COM>
2020-04-28 01:34:20 +0530

    vcn tf added

---
+++ vcn_create.tf
@@ -0,0 +1,15 @@
+provider "oci" {
+  version = ">= 3.0.0"
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaaksjyy2m5leuijdgxzxiy2j2li67l7bshh74n5hcparrhxr7iq7uq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaa7txfdrwsjur6lrrog2vcsvqd2mxag2awlvzgunkgidq3dacu7p6a"
+  fingerprint = "6b:5e:d9:07:b5:48:d2:23:1a:2b:60:18:dd:66:9a:cc"
+  private_key_path = "oci_api_key.pem"
+  region = "ap-mumbai-1"
+}
+
+resource "oci_core_virtual_network" "simple-vcn" {
+  cidr_block     = "10.0.0.0/16"
+  dns_label      = "vcn1"
+  compartment_id = "ocid1.compartment.oc1..aaaaaaaaa7vg63dona4ujp4urknt6bgps7fqnb3dlrwv5ygbcexcqodawerq"
+  display_name   = "simple-vcn"
+}
